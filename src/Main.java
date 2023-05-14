13d51f0c5a740cdc029d53ca06315568f278dcf6
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:17:29 +0530

    Added: elements of an array in reverse order

--- src/Main.java
+++ src/Main.java
@@ -84,6 +84,21 @@ public class Main {
     }
 
 
+    public static void ReverseOrder() {
+
+        int[] array = new int[] {1,2,3,4,5};
+        System.out.println("Array in order");
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+        System.out.println();
+        System.out.println("Array in Reverse order");
+        for(int i=array.length-1;i>=0;i--){
+            System.out.println(array[i]);
+        }
+    }
+
+
 
 
 }
