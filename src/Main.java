16f2938a7b364f36aa27cccdca21381e717503ef
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:13:25 +0530

    Added: elements of an array present on an even position

--- src/Main.java
+++ src/Main.java
@@ -73,5 +73,17 @@ public class Main {
 
 
 
+    public static void evenPosition() {
+        int[] array = new int[]{1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Element present on Even Position");
+
+        for (int i=1;i<array.length;i=i+2){
+            System.out.println(array[i]);
+        }
+
+    }
+
+
+
 
 }
