29d0688fa1dc2022e6f0afd1fb29ebe40113b48c
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:19:50 +0530

    Added: elements of an array present on odd position

--- src/Main.java
+++ src/Main.java
@@ -101,4 +101,16 @@ public class Main {
 
 
 
+    public static void oddPosition() {
+        int[] array = new int[]{1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Element present on Even Position");
+
+        for (int i=0;i<array.length;i=i+2){
+            System.out.println(array[i]);
+        }
+
+    }
+
+
+
 }
