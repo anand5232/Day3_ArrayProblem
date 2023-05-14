c51bac982f1b43e8ca7b0c6bcc753618230e028b
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:07:05 +0530

    Added: Largest element in array

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,19 @@ public class Main {
 
 
 
+    public static void largestElement() {
+
+        int[] array = new int[] {25,15,8,45,39};
+        int max = array[0];
+        for(int i=0;i<array.length;i++){
+
+            if(array[i]>max)
+                max=array[i];
+        }
+        System.out.println("Largest Element of array  = "+max);
+    }
+
+
+
 
 }
