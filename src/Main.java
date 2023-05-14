d30e5a1361d57dd262a9c674e15c5f053d7fd135
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:24:11 +0530

    Added:  elements of an array in ascending order

--- src/Main.java
+++ src/Main.java
@@ -125,5 +125,32 @@ public class Main {
     }
 
 
+    public static void AscendingOrder() {
+
+        int[] array = new int[]{3,7,1,5,2,4,6};
+        int temp=0;
+        System.out.println("Original Array");
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]>array[j]){
+                    temp = array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("array in ascending Order");
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+
+
+
 
 }
