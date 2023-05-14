51fffa5698e81cfdcc9d1ae22e476fc6672ce81c
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 19:59:08 +0530

    Added: Print Array Element

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,15 @@ public class Main {
 
 
     }
+
+    public static void PrintArrayElement() {
+
+        int[] array = new int[] {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Element of Array");
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+
+        }
+    }
+
 }
