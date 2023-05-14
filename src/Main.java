a08b004076b301045f0d1591d0bec064f44b40a1
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:22:01 +0530

    Added: duplicate elements of an array

--- src/Main.java
+++ src/Main.java
@@ -112,5 +112,18 @@ public class Main {
     }
 
 
+    public static void duplicateElement() {
+
+        int[] array = new int[]{1,2,3,4,2,7,8,8,3};
+        System.out.println("Duplicate element in array");
+        for (int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]==array[j])
+                    System.out.println(array[j]);
+            }
+        }
+    }
+
+
 
 }
