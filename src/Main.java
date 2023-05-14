ea6f197f827d99d3cf916a8d088141ed24614578
anand5232 <jhariyaanand000@gmail.com>
2023-05-14 20:28:11 +0530

    Added: 2nd Largest Number in an Array

--- src/Main.java
+++ src/Main.java
@@ -150,7 +150,28 @@ public class Main {
         }
     }
 
+    public static void secondLargestNumber() {
 
+        int[] array = new int[] {1,2,5,6,3,2};
+        int temp=0;
+
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+
+                if(array[i]>array[j]){
+                    temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+
+                }
+
+            }
+
+        }
+        System.out.println("Second Largest Number in array = "+array[array.length-2]);
+
+
+    }
 
 
 }
